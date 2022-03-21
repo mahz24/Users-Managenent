4c5b92200cfc4efa23d33d5e1d7ebb43d1b63535
Marco Zuñiga <marcohurtado358@gmail.com>
2022-03-21 12:13:33 -0500

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import UsersForm from "./components/UsersForm";
+import UsersList from "./components/UsersList";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [userEdit, setUserEdit] = useState(null);
+  useEffect(() => {
+    axios
+      .get("https://users-crud1.herokuapp.com/users/")
+      .then((res) => setUsers(res.data));
+  }, []);
+  const getUser = () => {
+    axios
+      .get("https://users-crud1.herokuapp.com/users/")
+      .then((res) => setUsers(res.data));
+  };
+
+  const deleteUser = (id) => {
+    axios
+      .delete(`https://users-crud1.herokuapp.com/users/${id}/`)
+      .then(() => getUser());
+  };
+
+  const addUser = (user) => {
+    axios
+      .post("https://users-crud1.herokuapp.com/users/", { ...user })
+      .then(() => {
+        getUser();
+      });
+  };
+
+  const userGrap = (user) => {
+    setUserEdit(user);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UsersList
+        className="user-list"
+        users={users}
+        deleteUser={deleteUser}
+        userGrap={userGrap}
+      />
+      <UsersForm
+        className="user-form"
+        addUser={addUser}
+        userEdit={userEdit}
+        getUser={getUser}
+      />
     </div>
   );
 }
